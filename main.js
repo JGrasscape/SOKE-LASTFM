a86c9b4285c57a0402df2d77f3db3ebdb0e029bb
Ruohoniemi Janne <99800597116@teonet.org>
2020-02-19 12:55:07 +0200

    tags and artist search

--- main.js
+++ main.js
@@ -7,32 +7,48 @@ var app = new Vue ({
         summary: '',  
         similarTitle: '',
         similars: '',
+        genreTitle: '',
+        tags: '',
         result: ''
     },
     methods: {
-        testApi: function() {
-            axios.get('http://ws.audioscrobbler.com/2.0/?method=artist.getinfo&artist=Cher&api_key=a9d080270634ad741de3ea9de4ab43a1&format=json').then(function (response) {
-              app.result = response.data;              
+        getArtist: function(artist) {
+            // Haetaan artistin tiedot Last.fm rajapinnasta
+            axios.get('http://ws.audioscrobbler.com/2.0/?method=artist.getinfo&artist='+artist+'&api_key=a9d080270634ad741de3ea9de4ab43a1&format=json').then(function (response) {
+                app.artistName = response.data.artist.name;
+                app.artistPic = response.data.artist.image[0]['#text'];
+                app.summary = response.data.artist.bio.summary;
+                app.genreTitle = 'Genret:';
+                app.tags = response.data.artist.tags.tag; 
+                app.similarTitle = 'Samankaltaisia artisteja:';
+                app.similars = response.data.artist.similar.artist;
             })
             .catch(function (error) {
-              app.result = 'Error ! Could not reach the API. ' + error;
+                app.result = 'Error ! Could not reach the API. ' + error;
             })
-        },
-        getArtist: function() {
-        axios.get('http://ws.audioscrobbler.com/2.0/?method=artist.getinfo&artist='+this.artist+'&api_key=a9d080270634ad741de3ea9de4ab43a1&format=json').then(function (response) {
-            app.artistName = response.data.artist.name;
-            app.artistPic = response.data.artist.image[0]['#text'];
-            app.summary = response.data.artist.bio.summary;
-            app.similarTitle = 'Samankaltaisia artisteja:';
-            app.similars = response.data.artist.similar.artist;            
-
-            app.result = response.data;              
-        })
-        .catch(function (error) {
-            app.result = 'Error ! Could not reach the API. ' + error;
-        })
+        },        
+        getGenre: function(genre) {
+            // Haetaan genret tiedot Last.fm rajapinnasta
+            axios.get('http://ws.audioscrobbler.com/2.0/?method=tag.getinfo&tag='+genre+'&api_key=a9d080270634ad741de3ea9de4ab43a1&format=json').then(function (response)
+            {
+                app.artistName = response.data.tag.name;
+                app.artistPic = '';
+                app.summary = response.data.tag.wiki.summary;
+                app.genreTitle = '';
+                app.tags = ''; 
+            })
+            .catch(function (error) {
+                app.result = 'Error ! Could not reach the API. ' + error;
+            })
+            // Haetaan genret artistit Last.fm rajapinnasta
+            axios.get('http://ws.audioscrobbler.com/2.0/?method=tag.gettopartists&tag='+genre+'&api_key=a9d080270634ad741de3ea9de4ab43a1&format=json').then(function (response)
+            {
+                app.similarTitle = 'Genren artisteja:';
+                app.similars = response.data.topartists.artist;
+            })
+            .catch(function (error) {
+                app.result = 'Error ! Could not reach the API. ' + error;
+            })
         }
     }
 });
-
-//https://lastfm.freetls.fastly.net/i/u/770x0/055b370c91da4ebba47ecc7aa3a37c5f.webp#055b370c91da4ebba47ecc7aa3a37c5f
