80bb25e4aee05925e645b201090588f2791fc7a1
Ruohoniemi Janne <99800597116@teonet.org>
2020-02-19 11:24:38 +0200

    lastfm api

--- main.js
+++ main.js
@@ -1,6 +1,38 @@
 var app = new Vue ({
     el: '#main',
     data: {   
-        title: "ads"
+        artist: '',
+        artistName: '',  
+        artistPic: '',    
+        summary: '',  
+        similarTitle: '',
+        similars: '',
+        result: ''
+    },
+    methods: {
+        testApi: function() {
+            axios.get('http://ws.audioscrobbler.com/2.0/?method=artist.getinfo&artist=Cher&api_key=a9d080270634ad741de3ea9de4ab43a1&format=json').then(function (response) {
+              app.result = response.data;              
+            })
+            .catch(function (error) {
+              app.result = 'Error ! Could not reach the API. ' + error;
+            })
+        },
+        getArtist: function() {
+        axios.get('http://ws.audioscrobbler.com/2.0/?method=artist.getinfo&artist='+this.artist+'&api_key=a9d080270634ad741de3ea9de4ab43a1&format=json').then(function (response) {
+            app.artistName = response.data.artist.name;
+            app.artistPic = response.data.artist.image[0]['#text'];
+            app.summary = response.data.artist.bio.summary;
+            app.similarTitle = 'Samankaltaisia artisteja:';
+            app.similars = response.data.artist.similar.artist;            
+
+            app.result = response.data;              
+        })
+        .catch(function (error) {
+            app.result = 'Error ! Could not reach the API. ' + error;
+        })
+        }
     }
 });
+
+//https://lastfm.freetls.fastly.net/i/u/770x0/055b370c91da4ebba47ecc7aa3a37c5f.webp#055b370c91da4ebba47ecc7aa3a37c5f
